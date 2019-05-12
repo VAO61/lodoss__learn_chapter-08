7f00948d1bd9ad40d295c236e214cd0b755b3071
unknown <iwork.mail@mail.ru>
2019-05-12 18:39:41 +0300

    UPD: modules _renamed names

--- _8__new-refactoring.js
+++ _8__new-refactoring.js
@@ -1,5 +1,5 @@
-const Manager = require('./modules/manager.js');
-const IncomingData = require('./modules/incomingdata.js');
+const Manager = require('./app/Manager.js');
+const IncomingData = require('./app/IncomingData.js');
 
 const manager = new Manager();
 const incomingData = new IncomingData(manager, 20);
