b61ad048ae4ca18bcc0752fecc1baf98097e6744
Unknown <iwork.mail@mail.ru>
2019-06-20 01:32:45 +0300

    UPD: Department.test

--- app/department/Department.js
+++ app/department/Department.js
@@ -24,7 +24,6 @@ class Department {
 
   firedDevelopers() {
     let developer = this.getUnBusyDeveloper();
-    console.log(developer);
 
     if (developer) {
       this.developers = this.developers.filter(item => {
--- test/Department.test.js
+++ test/Department.test.js
@@ -1,46 +1,115 @@
 const Department = require('../app/department/Department');
+const Developer = require('../app/developers/Developer');
+const Project = require('../app/projects/Project');
 
 describe('class Department', () => {
-  let department = new Department();
+  describe('method getUnBusyDevelopers', () => {
+    test('должен вернуть массив с двумя элементами', () => {
+      const department = new Department();
+      const project = new Project();
+      const developer1 = new Developer();
+      const developer2 = new Developer();
+      const developer3 = new Developer();
+      developer2.startProject(project);
+      department.developers.push(developer1, developer2, developer3);
 
-  // describe('getUnBusyDevelopers', () => {
-  //   test('some desc', () => {
-  //     let developer1 = {
-  //       skill: 1,
-  //       projects: null,
-  //       unBusyCount: 5
-  //     };
-  //     let developer2 = {
-  //       skill: 2,
-  //       projects: null,
-  //       unBusyCount: 5
-  //     };
-  //     let developer3 = {
-  //       skill: 3,
-  //       projects: null,
-  //       unBusyCount: 0
-  //     };
-  //     let developers = [{ developer1 }, { developer2 }, { developer3 }];
-  //     for (let i = 0; i < developers.length; i++) {
-  //       let getUnBusyDevelopers = developers.filter(
-  //         developers[i].unBusyCount !== 0
-  //       );
-  //       expect(getUnBusyDevelopers.length).toBe(2);
-  //     }
-  //   });
-  // });
+      const unBusyDevelopers = department.getUnBusyDevelopers();
 
-  describe('', () => {
-    test('', () => {
-      // let developer = { skill: 1, projects: null, unBusyCount: 5 };
-      // let developer1 = { skill: 1, projects: null, unBusyCount: 0 };
-      // let developer2 = { skill: 2, projects: null, unBusyCount: 0 };
-      // let developers = [{ developer }, { developer1 }, { developer2 }];
-      // developers = developers.filter(item => {
-      //   return item !== developer;
-      // });
-      // expect(department.statisticFiredDevelopers).toBe(1);
-      // TODO: разобраться с входящими данными!
+      expect(unBusyDevelopers.length).toBe(2);
+      expect(unBusyDevelopers).toEqual([developer1, developer3]);
+    });
+
+    test('должен вернуть пустой массив', () => {
+      const department = new Department();
+      const project = new Project();
+      const developer1 = new Developer();
+      const developer2 = new Developer();
+      const developer3 = new Developer();
+      developer1.startProject(project);
+      developer2.startProject(project);
+      developer3.startProject(project);
+      department.developers.push(developer1, developer2, developer3);
+
+      const unBusyDevelopers = department.getUnBusyDevelopers();
+
+      expect(unBusyDevelopers.length).toBe(0);
+    });
+  });
+
+  describe('method getUnBusyDeveloper', () => {
+    test('должен вернуть элемент', () => {
+      const department = new Department();
+      const project = new Project();
+      const developer1 = new Developer();
+      const developer2 = new Developer();
+      const developer3 = new Developer();
+      developer2.startProject(project);
+      developer1.skill = 1;
+      developer3.skill = 5;
+      department.developers.push(developer1, developer2, developer3);
+
+      const unBusyDeveloper = department.getUnBusyDeveloper();
+
+      expect(unBusyDeveloper).toBeInstanceOf(Developer);
+      expect(unBusyDeveloper).toBe(developer1);
+    });
+
+    test('должен вернуть undefined', () => {
+      const department = new Department();
+      const project = new Project();
+      const developer1 = new Developer();
+      const developer2 = new Developer();
+      const developer3 = new Developer();
+      developer1.startProject(project);
+      developer2.startProject(project);
+      developer3.startProject(project);
+      department.developers.push(developer1, developer2, developer3);
+
+      const unBusyDeveloper = department.getUnBusyDeveloper();
+
+      expect(typeof unBusyDeveloper).toBe('undefined');
+    });
+  });
+
+  describe('method firedDevelopers', () => {
+    test('должен уволить developer1', () => {
+      const department = new Department();
+      const project = new Project();
+      const developer1 = new Developer();
+      const developer2 = new Developer();
+      const developer3 = new Developer();
+      developer2.startProject(project);
+      developer1.skill = 1;
+      developer3.skill = 5;
+      department.developers.push(developer1, developer2, developer3);
+
+      department.firedDevelopers();
+
+      expect(department.developers.length).toBe(2);
+      expect(department.developers).toEqual([developer2, developer3]);
+      expect(department.statisticFiredDevelopers).toBe(1);
+    });
+
+    test('не должен увольнять', () => {
+      const department = new Department();
+      const project = new Project();
+      const developer1 = new Developer();
+      const developer2 = new Developer();
+      const developer3 = new Developer();
+      developer1.startProject(project);
+      developer2.startProject(project);
+      developer3.startProject(project);
+      department.developers.push(developer1, developer2, developer3);
+
+      department.firedDevelopers();
+
+      expect(department.developers.length).toBe(3);
+      expect(department.developers).toEqual([
+        developer1,
+        developer2,
+        developer3
+      ]);
+      expect(department.statisticFiredDevelopers).toBe(0);
     });
   });
 });
