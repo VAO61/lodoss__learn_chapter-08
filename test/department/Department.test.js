58a48bad1f9c78da57393424b024aac1f6761421
VAO61 <vao.frontend@gmail.com>
2019-06-21 17:32:24 +0300

    UPD: Department.test

--- test/department/Department.test.js
+++ test/department/Department.test.js
@@ -112,4 +112,119 @@ describe('class Department', () => {
       expect(department.statisticFiredDevelopers).toBe(0);
     });
   });
+
+  describe('method getNotStartProjects', () => {
+    test('should receive only one pending to take in dev project (project3)', () => {
+      const department = new Department();
+      const developer1 = new Developer();
+      const developer2 = new Developer();
+      const developer3 = new Developer();
+      const project1 = new Project();
+      const project2 = new Project();
+      const project3 = new Project();
+      department.developers.push(developer1, developer2, developer3);
+      department.projects.push(project1, project2, project3);
+      developer1.project = project1;
+      developer2.project = project1;
+      developer3.project = project2;
+
+      const unBusyDevelopers = department.getUnBusyDevelopers();
+      const notStartProjects = department.getNotStartProjects();
+
+      expect(unBusyDevelopers.length).toBe(0);
+      expect(notStartProjects.length).toBe(1);
+      expect(notStartProjects).toEqual([project3]);
+    });
+
+    test("shouldn't receive anyone project", () => {
+      const department = new Department();
+      const developer1 = new Developer();
+      const developer2 = new Developer();
+      const developer3 = new Developer();
+      const project1 = new Project();
+      const project2 = new Project();
+      const project3 = new Project();
+      department.developers.push(developer1, developer2, developer3);
+      department.projects.push(project1, project2, project3);
+      developer1.project = project1;
+      developer2.project = project2;
+      developer3.project = project3;
+
+      const unBusyDevelopers = department.getUnBusyDevelopers();
+      const notStartProjects = department.getNotStartProjects();
+
+      expect(unBusyDevelopers.length).toBe(0);
+      expect(notStartProjects.length).toBe(0);
+    });
+  });
+
+  describe('method workAssign', () => {
+    test('should assign project to unBusy developer (developer4)', () => {
+      /**
+       * Писать дополнительный тест для большего значения незанятых разработчиков, чем свободных проектов нецелесообразно.
+       * Причина: логический оператор '&&' в условии назначения проекта
+       */
+      const department = new Department();
+      const developer1 = new Developer();
+      const developer2 = new Developer();
+      const developer3 = new Developer();
+      const developer4 = new Developer();
+      const project1 = new Project();
+      const project2 = new Project();
+      const project3 = new Project();
+      const project4 = new Project();
+      department.developers.push(
+        developer1,
+        developer2,
+        developer3,
+        developer4
+      );
+      department.projects.push(project1, project2, project3, project4);
+      // developer1.project = project1;
+      developer1.startProject(project1);
+      // developer2.project = project1;
+      developer2.startProject(project2);
+      // developer3.project = project2;
+      developer3.startProject(project2);
+
+      const unBusyDevelopers = department.getUnBusyDevelopers();
+      const notStartProjects = department.getNotStartProjects();
+      while (notStartProjects.length && unBusyDevelopers.length) {
+        const project = notStartProjects.pop();
+        const developer = unBusyDevelopers.pop();
+        developer.startProject(project);
+      }
+
+      expect(unBusyDevelopers.length).toBe(0);
+      expect(developer4.project !== null).toBe(true);
+      expect(developer4.project).toEqual(project4);
+      expect(notStartProjects.length).toBe(1);
+    });
+
+    test("shouldn't assign anyone project", () => {
+      /**
+       * Собственно, как и этот
+       */
+      const department = new Department();
+      const developer1 = new Developer();
+      const developer2 = new Developer();
+      const project1 = new Project();
+      const project2 = new Project();
+      department.developers.push(developer1, developer2);
+      department.projects.push(project1, project2);
+      developer1.startProject(project1);
+      developer2.startProject(project2);
+
+      const unBusyDevelopers = department.getUnBusyDevelopers();
+      const notStartProjects = department.getNotStartProjects();
+      while (notStartProjects.length && unBusyDevelopers.length) {
+        const project = notStartProjects.pop();
+        const developer = unBusyDevelopers.pop();
+        developer.startProject(project);
+      }
+
+      expect(unBusyDevelopers.length).toBe(0);
+      expect(notStartProjects.length).toBe(0);
+    });
+  });
 });
